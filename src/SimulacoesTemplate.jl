8fa412f3e76d2ba62893719455629bcb6b30328b
Leandro Martinez <leandro@iqm.unicamp.br>
2020-12-04 08:29:43 -0300

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.10

---
+++ src/SimulacoesTemplate.jl
@@ -0,0 +1,5 @@
+module SimulacoesTemplate
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SimulacoesTemplate
+using Test
+
+@testset "SimulacoesTemplate.jl" begin
+    # Write your tests here.
+end
