a0edc2aeea7771a062c10392ddb5e743406d8afc
Leandro Martinez <leandro@iqm.unicamp.br>
2021-01-08 11:50:44 -0300

    fixed forcepair

--- src/forcepair.jl
+++ src/forcepair.jl
@@ -1,5 +1,6 @@
+#
 # Potential energy and force
-
+#
 function forcepair(x,y,r,data) 
   r6 = r^6
   r12 = r6^2
@@ -10,11 +11,12 @@ function forcepair(x,y,r,data)
   drdx2 = -(x[2]-y[2])/r
 
   sigfac1 = -12*(data.sig12/r13)
-  sigfac2 = 6*(data.sig6/r7)
+  sigfac2 = -6*(data.sig6/r7)
+
+  dfacdr = -data.eps4*(sigfac1 - sigfac2)
 
   upair = data.eps4*(data.sig12/r12 - data.sig6/r6)  
-  fx = ( -data.eps4*(sigfac1 + sigfac2)*drdx1, 
-         -data.eps4*(sigfac2 + sigfac2)*drdx2 ) 
+  fx = ( -dfacdr*drdx1, -dfacdr*drdx2 ) 
 
   return upair, fx
 end
